1e7755a667c06221d76b1442cdf1d8bbb0e59485
Arfeen Mushtaque <arfeenmushtaque1998@gmail.com>
2022-09-02 21:10:13 +0530

    s

--- index.js
+++ index.js
@@ -9,15 +9,15 @@ res.send("welcome to home page")
 
 app.post("/translate", (req, res)=>{
     console.log(req.body)
-    const {fromLanguage, toLanguage, sentence} = req.body
+    const { sentence} = req.body
     const tranObj = {
         a: 1,
         b: '1',
         c: sentence,
-        d: [true, 'true', fromLanguage, { a: 'hello', b: ['world']}],
+        d: [true, 'true', 'en', { a: 'hello', b: ['world']}],
     }
-    translate(tranObj, {to: toLanguage, except:['a']}).then(data => {
-              res.send(data)
+    translate(tranObj, {to: 'hi', except:['a']}).then(data => {
+              res.send(data.c)
           }).catch(err => {
              res.send(err)
     })
